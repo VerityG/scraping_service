537b64ebd2b2bb62cb426329642e92976ab3db1d
Ivan Vasiliuk <ivan.vasiliuk@gmail.com>
2020-09-22 22:05:08 +0300

    Project scraping, working

--- run_scraping.py
+++ run_scraping.py
@@ -1,6 +1,7 @@
 import codecs
 import os, sys
 
+
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
 os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
@@ -8,6 +9,8 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 import django
 django.setup()
 
+
+from django.db import DatabaseError
 from scraping.parsers import *
 from scraping.models import Vacancy, City, Programming_language
 
@@ -16,13 +19,20 @@ parsers = (
     (trudbox, 'http://trudbox.by/minsk?whatQuery=Python+')
 )
 
-city = City.objects.filter(slug='minsk')
-a = 1
+city = City.objects.filter(slug='minsk').first()
+programming_language = Programming_language.objects.filter(slug='python').first()
 jobs, errors = [], []
 for func, url in parsers:
     j, e = func(url)
     jobs += j
     errors += e
+
+for job in jobs:
+    v = Vacancy(**job, city=city, programming_language=programming_language)
+    try:
+        v.save()
+    except DatabaseError:
+        pass
 
 h = codecs.open('work.txt', 'w', 'utf-8')
 h.write(str(jobs))
